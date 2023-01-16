7d916dd78f20325806bf15a8113f649e81379807
Patrick Kabuga <kabugap02@gmail.com>
2023-01-17 01:51:50 +0300

    Modified the user's table migration

--- migrations/sqls/20230116002154-users-table-up.sql
+++ migrations/sqls/20230116002154-users-table-up.sql
@@ -1,1 +1,6 @@
-CREATE TABLE users (id SERIAL PRIMARY KEY, name VARCHAR(50), hash_password text);
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    hash_password text
+);
