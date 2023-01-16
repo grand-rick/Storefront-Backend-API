e84d4d94c5282b2e690bb1c10c2dcd59866807ba
Patrick Kabuga <kabugap02@gmail.com>
2023-01-16 18:39:03 +0300

    Added the test database

--- src/database.ts
+++ src/database.ts
@@ -3,7 +3,7 @@ import { Pool } from 'pg';
 
 dotenv.config();
 
-const { POSTGRES_HOST, POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD, ENV } =
+const { POSTGRES_HOST, POSTGRES_DB, POSTGRES_TEST_DB, POSTGRES_USER, POSTGRES_PASSWORD, ENV } =
 	process.env;
 
 let db = new Pool({
