c13fac833d74600e93728754d19dd2a37c4a72fe
Patrick Kabuga <kabugap02@gmail.com>
2023-01-16 03:10:43 +0300

    Finished setting up the server file

---
+++ src/server.ts
@@ -0,0 +1,27 @@
+import express, {Request, Response} from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: express.Application = express();
+const port = process.env.PORT;
+const corsOptions = {
+	origin: '*',
+	optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+app.use(morgan('common'));
+app.use(bodyParser.json());
+
+
+app.get('/', (req: Request, res: Response) => {
+    res.json('This is the Homepage');
+});
+
+app.listen(port, () => {
+    console.log(`Server started on http://localhost${port}`);
+});
