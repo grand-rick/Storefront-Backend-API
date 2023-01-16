667d8156a485c0ba390c8dc87cbe3f442ac88de6
Patrick Kabuga <kabugap02@gmail.com>
2023-01-16 04:13:53 +0300

    linted and prettified the code

--- src/handlers/users.ts
+++ src/handlers/users.ts
@@ -1,80 +1,80 @@
-import express, {Request, Response} from 'express';
-import UserStore, {User} from '../models/user';
+import express, { Request, Response } from 'express';
+import UserStore, { User } from '../models/user';
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const store = new UserStore();
-const TOKEN_SECRET = process.env.TOKEN_SECRET as unnown as string;
+const TOKEN_SECRET = process.env.TOKEN_SECRET as unknown as string;
 
 const index = async (_req: Request, res: Response) => {
-    try {
-        const users = await store.index();
-        res.json(users);
-    } catch (err) {
-        res.status(400);
-        res.json(err);
-    }
-}
+	try {
+		const users = await store.index();
+		res.json(users);
+	} catch (err) {
+		res.status(400);
+		res.json(err);
+	}
+};
 
 const create = async (req: Request, res: Response) => {
-    const user1: User = {
-        name: req.body.name,
-        password: req.body.password
-    }
-    try {
-        const newUser = await store.index();
-        const token = jwt.sign({user: newUser}, TOKEN_SECRET);
-        res.json(token);
-    } catch (err) {
-        res.status(400);
-        res.json(err);
-    }
-}
+	const user1: User = {
+		name: req.body.name,
+		password: req.body.password,
+	};
+	try {
+		const newUser = await store.create(user1);
+		const token = jwt.sign({ user: newUser }, TOKEN_SECRET);
+		res.json(token);
+	} catch (err) {
+		res.status(400);
+		res.json(err);
+	}
+};
 
 const show = async (req: Request, res: Response) => {
-    try {
-        const user = await store.show(req.params.id);
-        res.json(user);
-    } catch (err) {
-        res.status(400);
-        res.json(err);
-    }
-}
+	try {
+		const user = await store.show(req.params.id);
+		res.json(user);
+	} catch (err) {
+		res.status(400);
+		res.json(err);
+	}
+};
 
 const update = async (req: Request, res: Response) => {
-    const user1: User = {
-        id: req.params.id,
-        name: req.body.name,
-        password: req.body.password
-    }
-    try {
-        const updatedUser = await store.update(user1);
-        const token = jwt.sign({user: updatedUser}, TOKEN_SECRET);
-        res.json(token);
-    } catch (err) {
-        res.status(400);
-        res.json(err);
-    }
-}
+	const user1: User = {
+		id: req.params.id,
+		name: req.body.name,
+		password: req.body.password,
+	};
+	try {
+		const updatedUser = await store.update(user1);
+		const token = jwt.sign({ user: updatedUser }, TOKEN_SECRET);
+		res.json(token);
+	} catch (err) {
+		res.status(400);
+		res.json(err);
+	}
+};
 
 const destroy = async (req: Request, res: Response) => {
-    try {
-        const deleted = await store.show(req.params.id);
-        res.json(deleted);
-    } catch (err) {
-        res.status(400);
-        res.json(err);
-    }
-}
+	try {
+		const deleted = await store.show(req.params.id);
+		res.json(deleted);
+	} catch (err) {
+		res.status(400);
+		res.json(err);
+	}
+};
 
 const usersRoutes = (app: express.Application) => {
-    app.post('/signup', create);
-    app.get('/users', index);
-    app.get('/users/:id', show);
-    app.put('/users/:id', update);
-    app.delete('/users/:id', destroy);
-}
+	app.post('/signup', create);
+	app.get('/users', index);
+	app.get('/users/:id', show);
+	app.put('/users/:id', update);
+	app.delete('/users/:id', destroy);
+};
 
 export default usersRoutes;
--- src/models/user.ts
+++ src/models/user.ts
@@ -10,7 +10,7 @@ const saltRounds = process.env.SALT_ROUNDS as unknown as string;
 export type User = {
 	id?: string | number;
 	name: string;
-    password: string;
+	password: string;
 };
 
 export default class UserStore {
@@ -23,67 +23,69 @@ export default class UserStore {
 			const users = result.rows;
 			return users;
 		} catch (err) {
-            throw new Error(`Unable to show all users. Error ${err}`);
+			throw new Error(`Unable to show all users. Error ${err}`);
 		}
 	}
 
-    async create(u: User): Promise<User> {
-        try {
-            const sql = 'INSERT INTO users (name, hash_password) VALUES ($1, $2) RETURNING *';
-            const hash = bcrypt.hashSync(
-                `${u.password}${pepper}`,
-                parseInt(saltRounds)
-            );
-            const conn = await db.connect();
-            const result = await conn.query(sql, [u.name, hash]);
-            conn.release();
-            const newUser = result.rows[0];
-            return newUser;
-        } catch (err) {
-            throw new Error(`Unable to create user. Error ${err}`);
-        }
-    }
+	async create(u: User): Promise<User> {
+		try {
+			const sql =
+				'INSERT INTO users (name, hash_password) VALUES ($1, $2) RETURNING *';
+			const hash = bcrypt.hashSync(
+				`${u.password}${pepper}`,
+				parseInt(saltRounds)
+			);
+			const conn = await db.connect();
+			const result = await conn.query(sql, [u.name, hash]);
+			conn.release();
+			const newUser = result.rows[0];
+			return newUser;
+		} catch (err) {
+			throw new Error(`Unable to create user. Error ${err}`);
+		}
+	}
 
-    async update(u: User): Promise<User> {
-        try {
-            const sql = 'UPDATE users SET name = $2, hash_password = $3 WHERE id = $1';
-            const hash = bcrypt.hashSync(
-                `${u.password}${pepper}`,
-                parseInt(saltRounds)
-            );
-            const conn = await db.connect();
-            const result = await conn.query(sql, [u.id, u.name, hash]);
-            conn.release();
-            const updatedUser = result.rows[0];
-            return updatedUser;
-        } catch (err) {
-            throw new Error(`Unable to create user. Error ${err}`);
-        }
-    }
+	async update(u: User): Promise<User> {
+		try {
+			const sql =
+				'UPDATE users SET name = $2, hash_password = $3 WHERE id = $1';
+			const hash = bcrypt.hashSync(
+				`${u.password}${pepper}`,
+				parseInt(saltRounds)
+			);
+			const conn = await db.connect();
+			const result = await conn.query(sql, [u.id, u.name, hash]);
+			conn.release();
+			const updatedUser = result.rows[0];
+			return updatedUser;
+		} catch (err) {
+			throw new Error(`Unable to create user. Error ${err}`);
+		}
+	}
 
-    async show(id: string)Promise<User> {
-        try {
-            const sql = 'SELECT * FROM users WHERE id = $1';
-            const conn = await db.connect();
-            const result = await conn.query(sql, [id]);
-            conn.release();
-            const user = result.rows[0];
-            return user;
-        } catch (err) {
-            throw new Error(`Unable to show user. Error ${err}`);
-        }
-    }
+	async show(id: string): Promise<User> {
+		try {
+			const sql = 'SELECT * FROM users WHERE id = $1';
+			const conn = await db.connect();
+			const result = await conn.query(sql, [id]);
+			conn.release();
+			const user = result.rows[0];
+			return user;
+		} catch (err) {
+			throw new Error(`Unable to show user. Error ${err}`);
+		}
+	}
 
-    async delete(id: string)Promise<User> {
-        try {
-            const sql = 'DELETE FROM users WHERE id = $1';
-            const conn = await db.connect();
-            const result = await conn.query(sql, [id]);
-            conn.release();
-            const deleted = result.rows[0];
-            return deleted;
-        } catch (err) {
-            throw new Error(`Unable to delete user. Error ${err}`);
-        }
-    }
+	async delete(id: string): Promise<User> {
+		try {
+			const sql = 'DELETE FROM users WHERE id = $1';
+			const conn = await db.connect();
+			const result = await conn.query(sql, [id]);
+			conn.release();
+			const deleted = result.rows[0];
+			return deleted;
+		} catch (err) {
+			throw new Error(`Unable to delete user. Error ${err}`);
+		}
+	}
 }
