9461778ec1cb5d126f190f5c241adbf1d6afad28
Patrick Kabuga <kabugap02@gmail.com>
2023-01-16 03:37:40 +0300

    Formatted the code

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express, {Request, Response} from 'express';
+import express, { Request, Response } from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
@@ -17,11 +17,10 @@ app.use(cors(corsOptions));
 app.use(morgan('common'));
 app.use(bodyParser.json());
 
-
 app.get('/', (req: Request, res: Response) => {
-    res.json('This is the Homepage');
+	res.json('This is the Homepage');
 });
 
 app.listen(port, () => {
-    console.log(`Server started on http://localhost${port}`);
+	console.log(`Server started on http://localhost${port}`);
 });
