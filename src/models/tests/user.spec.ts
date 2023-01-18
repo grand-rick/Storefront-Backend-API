7e2f7bcf8146fffc4e60feb414caec5c160eb4ca
Patrick Kabuga <kabugap02@gmail.com>
2023-01-18 19:23:00 +0300

    Wrote the user tests

---
+++ src/models/tests/user.spec.ts
@@ -0,0 +1,52 @@
+import UserStore, { User } from '../user';
+
+const store = new UserStore();
+
+describe('User Model', () => {
+	it('should have an index method', () => {
+		expect(store.index).toBeDefined();
+	});
+
+	it('should have a show method', () => {
+		expect(store.show).toBeDefined();
+	});
+
+	it('should have a create method', () => {
+		expect(store.create).toBeDefined();
+	});
+
+	it('should have a delete method', () => {
+		expect(store.delete).toBeDefined();
+	});
+
+	it('create method should add a user', async () => {
+		const result: User = await store.create({
+			first_name: 'Some',
+			last_name: 'Name',
+            username: 'Test User',
+            hash_password: 'randomPassword'
+		});
+		expect(result).toBeDefined();
+	});
+
+	it('index method should return a list of users', async () => {
+		const result = await store.index();
+		expect(result).toBeDefined();
+	});
+
+	it('show method should return the correct user', async () => {
+		const result = await store.show('1');
+		expect(result).toBeDefined();
+	});
+
+	it('delete method should remove the user', async () => {
+		await store.delete('1');
+		const result = await store.index();
+		expect(result).toEqual([]);
+	});
+
+	it('authenticate method works', async () => {
+		const result = await store.authenticate('Test User', 'randomPassword');
+		expect(result).toBeDefined();
+	});
+});
