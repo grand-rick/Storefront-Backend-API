4c753a9f77e0fe2d4a07a6e5c16d78eba919056c
Patrick Kabuga <kabugap02@gmail.com>
2023-01-18 18:44:52 +0300

    Wrote the product model's test

---
+++ src/models/tests/product.spec.ts
@@ -0,0 +1,59 @@
+import ProductStore, { Product } from '../product';
+
+const store = new ProductStore();
+
+describe('Product Model', () => {
+	it('should have an index method', () => {
+		expect(store.index).toBeDefined();
+	});
+
+	it('should have a show method', () => {
+		expect(store.show).toBeDefined();
+	});
+
+	it('should have a create method', () => {
+		expect(store.create).toBeDefined();
+	});
+
+	it('should have a delete method', () => {
+		expect(store.delete).toBeDefined();
+	});
+
+	it('create method should add a book', async () => {
+		const result: Product = await store.create({
+			name: 'Bridge',
+			price: '250'
+		});
+		expect(result).toEqual({
+			id: 1,
+			name: 'Bridge',
+			price: '250',
+		});
+	});
+
+	it('index method should return a list of books', async () => {
+		const result = await store.index();
+		expect(result).toEqual([
+			{
+				id: 1,
+				name: 'Bridge',
+				price: '250',
+			}
+		]);
+	});
+
+	it('show method should return the correct product', async () => {
+		const result = await store.show('1');
+		expect(result).toEqual({
+			id: 1,
+			name: 'Bridge',
+			price: '250',
+		});
+	});
+
+	it('delete method should remove the product', async () => {
+		await store.delete('1');
+		const result = await store.index();
+		expect(result).toEqual([]);
+	});
+});
