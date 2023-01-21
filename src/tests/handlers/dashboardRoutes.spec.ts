a5865b2005b4b38b19b95c16577252ef475950ca
Patrick Kabuga <kabugap02@gmail.com>
2023-01-22 02:32:49 +0300

    Wrote tests for my dashboard routes

---
+++ src/tests/handlers/dashboardRoutes.spec.ts
@@ -0,0 +1,28 @@
+import supertest from 'supertest';
+import app from '../../server';
+import token from './1orders.spec';
+
+const request = supertest(app);
+
+describe("Testing the dashboard routes handler functions", () => {
+	it(`GET /products_in_orders should show all products in sorted by first order`, async () => {
+		const response = await request
+			.get('/products_in_orders')
+			.set('Authorization', `Bearer ${token}`);
+		expect(response.status).toEqual(200);
+	});
+
+	it(`GET /expensive_products should show top five expensive products`, async () => {
+		const response = await request
+			.get('/expensive_products')
+			.set('Authorization', `Bearer ${token}`);
+		expect(response.status).toEqual(200);
+	});
+
+	it(`GET /users_with_active_orders should show users with active orders`, async () => {
+		const response = await request
+			.get('/users_with_active_orders')
+			.set('Authorization', `Bearer ${token}`);
+		expect(response.status).toEqual(200);
+	});
+});
