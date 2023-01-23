65b6ddf2e367f1e8f9b7758b1e5774964525328f
Patrick Kabuga <kabugap02@gmail.com>
2023-01-24 01:41:57 +0300

    Added tests to cover all order model methods

--- src/tests/models/3order.spec.ts
+++ src/tests/models/3order.spec.ts
@@ -6,6 +6,23 @@ const store = new OrderStore();
 const user_store = new UserStore();
 const product_store = new ProductStore();
 
+const user1: User = {
+	first_name: 'Some',
+	last_name: 'Name',
+	username: 'Test User',
+	hash_password: 'randomPassword',
+};
+const product1: Product = {
+	name: 'Bed',
+	price: '500',
+};
+const order1: Order = {
+	status: 'active',
+	product_quantity: '50',
+	product_id: '4',
+	user_id: '4',
+};
+
 describe('Order Model', () => {
 	it('should have an index method', () => {
 		expect(store.index).toBeDefined();
@@ -20,22 +37,6 @@ describe('Order Model', () => {
 	});
 
 	it('create method should add an order', async () => {
-		const user1: User = {
-			first_name: 'Some',
-			last_name: 'Name',
-			username: 'Test User',
-			hash_password: 'randomPassword',
-		};
-		const product1: Product = {
-			name: 'Bed',
-			price: '500',
-		};
-		const order1: Order = {
-			status: 'active',
-			product_quantity: '50',
-			product_id: '4',
-			user_id: '4',
-		};
 		const user = await user_store.create(user1);
 		const product = await product_store.create(product1);
 		const newOrder: Order = await store.create(order1);
@@ -70,6 +71,19 @@ describe('Order Model', () => {
 			product_id: 4,
 			user_id: 4,
 		});
+	});
+
+	it('addProduct method should add products in orders', async () => {
+		const quantity = order1.product_quantity;
+		const orderId = '2';
+		const productId = '4';
+		const result = await store.addProduct(quantity, orderId, productId);
+		expect(result).toBeDefined();
+	});
+
+	it('deleteProduct method should delete products in orders', async () => {
+		const result = await store.deleteProduct('4');
+		expect(result).toEqual('Deleted')
 	});
 
 	it('delete method should remove the order', async () => {
